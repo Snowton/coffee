7f271e52c173373cf0d7a6569d753ffc4a31eb4c
Shreya Mogulothu <shreya.mogulothu@gmail.com>
2021-03-23 12:01:52 -0400

    testing git

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const app = express()
 app.set("view-engine", "ejs")
 app.use(express.urlencoded());
 
-
+app.get("/", (req, res) => {
+    res.send("glad to have you here");
+})
 
 app.listen(process.env.PORT || 3000, (err) => {
     if (!err) console.log("successfully started on port 3000 or process.env.PORT");
