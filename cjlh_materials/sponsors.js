6ad3584e65384e18fe8457a21d5de2e5efe936aa
Shreya Mogulothu <shreya.mogulothu@gmail.com>
2021-06-02 17:26:09 -0400

    add aops

--- cjlh_materials/sponsors.js
+++ cjlh_materials/sponsors.js
@@ -3,6 +3,10 @@ const sponsors = [
         name: "Wolfram Alpha",
         img: "wolframalpha.png"
     },
+    {
+        name: "Art of Problem Solving",
+        img: "aops.png"
+    },
 ]
 
 module.exports = sponsors;
