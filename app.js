0e2e0219d03def665573540438f1cde7f1b6dbd0
Shreya Mogulothu <shreya.mogulothu@gmail.com>
2022-01-23 22:51:24 -0500

    sanitize file names

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ const storage = multer.diskStorage({
     },
     filename: function (req, file, cb) {
       const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-      cb(null, file.originalname + '-' + uniqueSuffix)
+      cb(null, encodeURIComponent(file.originalname.toLowerCase().replace(/\s/g, "-")) + '-' + uniqueSuffix)
     }
 })
 const upload = multer({ storage: storage })
